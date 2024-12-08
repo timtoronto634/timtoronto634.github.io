0044e0ff2e18721dca43ce27e2b937e977a58dcc
timtoronto634 <tim225x@gmail.com>
2024-12-07 20:24:49 -0800

    add sample contents of Home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,3 +1,19 @@
+import { Typography, Stack } from '@mui/material';
+
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Stack
+      sx={{
+        alignItems: 'center',
+        direction: 'column',
+      }}
+    >
+      <Stack>Hello, I'm A2, Atsuhiro Uchida</Stack>
+      <Stack>
+        <Typography>X / Twitter</Typography>
+        <Typography>Linkedin</Typography>
+        <Typography>Github</Typography>
+      </Stack>
+    </Stack>
+  );
 };
