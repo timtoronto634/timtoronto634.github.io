d8fc0f82bf24aa4f3027e75ea31379113b4e07f9
timtoronto634 <tim225x@gmail.com>
2024-12-07 20:17:22 -0800

    add sample content of blogs

--- src/components/Blogs.tsx
+++ src/components/Blogs.tsx
@@ -1,3 +1,17 @@
+import { ListItem, Stack, Typography } from '@mui/material';
+
 export const Blogs = () => {
-  return <div>Blogs</div>;
+  return (
+    <Stack>
+      <Stack>
+        <Typography>Japanese</Typography>
+        <Stack>
+          <ListItem>【AWS re:Invent 2024】</ListItem>
+        </Stack>
+      </Stack>
+      <Stack>
+        <Typography>English</Typography>
+      </Stack>
+    </Stack>
+  );
 };
