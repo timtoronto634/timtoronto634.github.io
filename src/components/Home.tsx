91b0ef0330d6d5942f98721b74a8a6bc49be092b
timtoronto634 <tim225x@gmail.com>
2025-04-05 13:37:02 +0900

    feat: replace Twitter icon with X icon and update color scheme

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,7 @@
 import { Typography, Stack, Link, Avatar, Paper, IconButton } from '@mui/material';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import TwitterIcon from '@mui/icons-material/Twitter';
+import XIcon from '@mui/icons-material/X';
 import EmailIcon from '@mui/icons-material/Email';
 import generalInfo from '../data/general_info.yml';
 
@@ -55,9 +55,9 @@ const SocialLinks: React.FC<{ contacts: Contact[] }> = ({ contacts }) => {
           href={contact.twitter}
           target='_blank'
           rel='noopener noreferrer'
-          sx={{ color: '#1DA1F2' }}
+          sx={{ color: 'black' }}
         >
-          <TwitterIcon fontSize='large' />
+          <XIcon fontSize='large' />
         </IconButton>
       )}
       {contact.mail && (
